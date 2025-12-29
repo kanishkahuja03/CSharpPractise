316573efbe88f888b248794b10ab108b51a429f2
kanishkahuja03 <kanishk.ahuja@cctech.co.in>
2025-12-29 15:22:58 +0530

    LINQ to XML data transfer

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,13 +1,37 @@
 ﻿using Basics;
-    public class Program
+using System.Xml.Linq;
+public class Program
     {
     static void Main(string[] args)
     {
         //PremiumLibraryMember member = new PremiumLibraryMember("Alice Smith", 30);
         //member.DisplayMembershipDetails();
 
-        carMenu();
+        //carMenu();
 
+        xmlData();
+
+    }
+
+    public static void xmlData()
+    {
+        string inputFilePath = "input.xml";
+        string outputFilePath = "output.xml";
+
+        XElement inputXml = XElement.Load(inputFilePath);
+        // Reads the xml data and creates an in-memory representation of xml data.
+        // XElement is a part of LINQ to XML API in C#.
+
+        var modifiedData = from element in inputXml.Elements("Person")
+                           where (int)element.Element("Age") > 25
+                           select new XElement("ModifiedPerson",
+                           new XElement("Name", element.Element("Name").Value),
+                           new XElement("Age", (int)element.Element("Age") + 5)
+                           );
+
+        XElement outputXml = new XElement("ModifiedData", modifiedData);
+        outputXml.Save(outputFilePath);
+        Console.WriteLine("Data tranfer is done to {0}", outputFilePath);
     }
 
     public static void carMenu()
