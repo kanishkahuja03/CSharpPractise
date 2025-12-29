42a06e37a99110e3bb63d3cee55e7f4deae31e2f
kanishkahuja03 <kanishk.ahuja@cctech.co.in>
2025-12-29 12:53:47 +0530

    LINQ Example

---
+++ Basics/Lamda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Basics
+{
+    public class Lamda
+    {
+        //(int, string, double) Product = (ID: id, Name: name, Price: price);
+
+    }
+    
+
+}
---
+++ Basics/Linq.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public override string ToString() => $"{Year} {Make} {Model} - Color: {Color}";
+    };
+}
