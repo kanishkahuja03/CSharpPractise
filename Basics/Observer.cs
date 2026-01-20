fc963e7a0ae2852c0c5a31fc601751d3934affd9
kanishkahuja03 <kanishk.ahuja@cctech.co.in>
2026-01-20 14:44:49 +0530

    Behavioral :- Strategy and Observer Patterns

---
+++ Basics/Observer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public delegate void PriceChangedEventHandler(object sender, PriceChangedEventArgs args);
+
+    public class PriceChangedEventArgs : EventArgs
+    {
+        public int OldPrice { get; }
+        public int NewPrice { get; }
+
+        public PriceChangedEventArgs(int oldPrice, int newPrice)
+        {
+            OldPrice = oldPrice;
+            NewPrice = newPrice;
+        }
+    }
+    public class Stock
+    {
+        // .NET in-built event handler
+        //public event EventHandler<PriceChangedEventArgs> PriceChanged;
+        public event PriceChangedEventHandler PriceChanged;
+        private int _price;
+
+        public Stock(int initialPrice)
+        {
+            _price = initialPrice;
+        }
+
+        public void UpdatePrice(int newPrice)
+        {
+            if (_price == newPrice)
+                return;
+
+            int oldPrice = _price;
+            _price = newPrice;
+
+            OnPriceChanged(oldPrice, newPrice);
+        }
+        public virtual void OnPriceChanged(int oldPrice, int newPrice)
+        {
+            PriceChanged?.Invoke(this, new PriceChangedEventArgs(oldPrice, newPrice));
+        }
+    }
+
+    public class MobileApp
+    {
+        public void Subscribe(Stock stock)
+        {
+            stock.PriceChanged += OnPriceChanged;
+        }
+
+        public void Unsubscribe(Stock stock)
+        {
+            stock.PriceChanged -= OnPriceChanged;
+        }
+
+        private void OnPriceChanged(object sender, PriceChangedEventArgs e)
+        {
+            Console.WriteLine(
+                $"[Mobile] your stocks price changed from {e.OldPrice} to {e.NewPrice}"
+            );
+        }
+    }
+
+    public class WebApp
+    {
+        public void Subscribe(Stock stock)
+        {
+            stock.PriceChanged += OnPriceChanged;
+        }
+
+        public void Unsubscribe(Stock stock)
+        {
+            stock.PriceChanged -= OnPriceChanged;
+        }
+
+        private void OnPriceChanged(object sender, PriceChangedEventArgs e)
+        {
+            Console.WriteLine(
+                $"[Web] your stocks price changed from {e.OldPrice} to {e.NewPrice}"
+            );
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Stock appleStock = new Stock(150);
+
+            MobileApp mobileApp = new MobileApp();
+            WebApp webApp = new WebApp();
+
+            mobileApp.Subscribe(appleStock);
+            webApp.Subscribe(appleStock);
+
+            appleStock.UpdatePrice(155);
+            appleStock.UpdatePrice(160);
+
+            Console.WriteLine("Unsubscribing Web App");
+            webApp.Unsubscribe(appleStock);
+
+            appleStock.UpdatePrice(165);
+        }
+    }
+}
---
+++ Basics/Strategy.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public interface IExportStrategy
+    {
+        void Export(string data);
+    }
+    public class CsvExportStrategy : IExportStrategy
+    {
+        public void Export(string data)
+        {
+            Console.WriteLine($"Exporting data in CSV format: {data}");
+        }
+    }
+    public class JsonExportStrategy : IExportStrategy
+    {
+        public void Export(string data)
+        {
+            Console.WriteLine($"Exporting data in JSON format: {data}");
+        }
+    }
+    public class XmlExportStrategy : IExportStrategy
+    {
+        public void Export(string data)
+        {
+            Console.WriteLine($"Exporting data in XML format: {data}");
+        }
+    }
+    public class PdfExportStrategy : IExportStrategy
+    {
+        public void Export(string data)
+        {
+            Console.WriteLine($"Exporting data in PDF format: {data}");
+        }
+    }
+    public class DataExporterContext
+    {
+        private readonly IExportStrategy _exportStrategy;
+        public DataExporterContext(IExportStrategy exportStrategy)
+        {
+            _exportStrategy = exportStrategy;
+        }
+        public void ExportData(string data)
+        {
+            _exportStrategy.Export(data);
+        }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            string data = "Sample Data";
+            IExportStrategy csvStrategy = new CsvExportStrategy();
+            DataExporterContext csvExporter = new DataExporterContext(csvStrategy);
+            csvExporter.ExportData(data);
+            IExportStrategy jsonStrategy = new JsonExportStrategy();
+            DataExporterContext jsonExporter = new DataExporterContext(jsonStrategy);
+            jsonExporter.ExportData(data);
+            IExportStrategy xmlStrategy = new XmlExportStrategy();
+            DataExporterContext xmlExporter = new DataExporterContext(xmlStrategy);
+            xmlExporter.ExportData(data);
+            IExportStrategy pdfStrategy = new PdfExportStrategy();
+            DataExporterContext pdfExporter = new DataExporterContext(pdfStrategy);
+            pdfExporter.ExportData(data);
+        }
+    }
+}
