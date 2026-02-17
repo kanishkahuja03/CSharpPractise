8aeca6a91e6bdbf169b9af3350707b3587def3fe
kanishkahuja03 <kanishk.ahuja@cctech.co.in>
2026-02-17 14:24:08 +0530

    LogError file added + TestCase for it

---
+++ Basics.Tests/ErrorLogTests.cs
@@ -0,0 +1,19 @@
+﻿using IEnumerablePractise;
+using System.Linq;
+
+namespace Basics.Tests
+{
+    [TestClass]
+    public sealed class ErrorLogTests
+    {
+        [TestMethod]
+        public async Task Test_ErrorCount()
+        {
+            using var reader = new StreamReader("LogIEPractise.txt");
+            List<string> errorLog = await LogReader.GetErrorLog(reader).ToListAsync();
+            int errorCount = errorLog.Count();
+            int expectedErrors = 3;
+            Assert.AreEqual(expectedErrors, errorCount, 0.001, "Number of errors are incorrect.");
+        }
+    }
+}
---
+++ Basics/IEnumerablePractise.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace IEnumerablePractise
+{
+    public class LogReader
+    {
+        static public async IAsyncEnumerable<string> GetErrorLog(TextReader reader)
+        {
+            string? line;
+            while ((line = await reader.ReadLineAsync()) != null)
+            {
+                if (line.Contains("ERROR"))
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            using var reader = new StreamReader("LogIEPractise.txt");
+            using var writer = new StreamWriter("ErrorLog.txt");
+
+            await foreach (var line in LogReader.GetErrorLog(reader))
+            {
+                await writer.WriteLineAsync(line);
+            }
+
+        }
+    }
+}
